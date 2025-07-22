868c23ea7463c7cbb34bd08183ccf1d77d6e5822
liarsa.me <shepard@liarsa.me>
2025-07-23 07:16:25 +0800

    dev

--- apps/open-server/domain/llm-model/kimi.go
+++ apps/open-server/domain/llm-model/kimi.go
@@ -2,10 +2,14 @@ package llm
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"sync"
 
+	libqdrant "github.com/eviltomorrow/open-terminal/lib/qdrant"
 	"github.com/eviltomorrow/open-terminal/lib/snowflake"
 	"github.com/eviltomorrow/open-terminal/lib/zlog"
+	"github.com/qdrant/go-client/qdrant"
 	"github.com/sashabaranov/go-openai"
 	"go.uber.org/zap"
 )
@@ -32,23 +36,92 @@ func NewKimiClient(baseURL string, apiKey string) *KimiClient {
 }
 
 type KimiSession struct {
+	sync.RWMutex
+
 	Id        string
 	ModelName string
 
-	client *KimiClient
+	client       *KimiClient
+	alreadyStart bool
 }
 
-func (c *KimiClient) NewSession(modelName string) *KimiSession {
-	id := snowflake.GenerateID()
+func (c *KimiClient) NewSession(modelName string) (*KimiSession, error) {
+	id := fmt.Sprintf("Kimi-%v", snowflake.GenerateID())
 
 	session := &KimiSession{
 		Id:        id,
 		ModelName: modelName,
 	}
-	return session
+	return session, nil
+}
+
+func (s *KimiSession) isAlreadyStart() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.alreadyStart
+}
+
+func (s *KimiSession) setAlreadyStart() {
+	s.Lock()
+	defer s.Unlock()
+
+	s.alreadyStart = true
+}
+
+func (s *KimiSession) embeddings(content string) ([]float32, error) {
+	resp, err := s.client.ai.CreateEmbeddings(context.Background(),
+		openai.EmbeddingRequest{
+			Model: openai.AdaEmbeddingV2,
+			Input: []string{content},
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Data) > 0 {
+		return resp.Data[0].Embedding, nil
+	}
+	return nil, fmt.Errorf("panic: no embeddings result")
+}
+
+func (s *KimiSession) cache(id uint64, content string) error {
+	vec, err := s.embeddings(content)
+	if err != nil {
+		return err
+	}
+
+	point := &qdrant.PointStruct{
+		Id:      qdrant.NewIDNum(id),
+		Vectors: qdrant.NewVectors(vec...),
+		Payload: qdrant.NewValueMap(map[string]interface{}{"content": content}),
+	}
+
+	_, err = libqdrant.Client.Upsert(context.Background(), &qdrant.UpsertPoints{
+		CollectionName: s.Id,
+		Points:         []*qdrant.PointStruct{point},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *KimiSession) StartChat(ctx context.Context, content string, opts ...func(*openai.ChatCompletionRequest)) (chan string, error) {
+	if s.isAlreadyStart() {
+		return nil, fmt.Errorf("session'chat already start")
+	}
+
+	if err := libqdrant.Client.CreateCollection(context.Background(), &qdrant.CreateCollection{
+		CollectionName: s.Id,
+		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
+			Size:     1536,
+			Distance: qdrant.Distance_Cosine,
+		}),
+	}); err != nil {
+		return nil, err
+	}
+
 	req := openai.ChatCompletionRequest{
 		Model:  s.ModelName,
 		Stream: true,
@@ -65,7 +138,13 @@ func (s *KimiSession) StartChat(ctx context.Context, content string, opts ...fun
 		opt(&req)
 	}
 
-	return s.sendRequest(ctx, req)
+	ch, err := s.sendRequest(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	s.setAlreadyStart()
+	return ch, nil
 }
 
 func (s *KimiSession) Send(ctx context.Context, content string, opts ...func(*openai.ChatCompletionRequest)) (chan string, error) {
@@ -111,6 +190,7 @@ func (s *KimiSession) sendRequest(ctx context.Context, req openai.ChatCompletion
 				ch <- delta
 			}
 		}
+
 		close(ch)
 		resp.Close()
 	}()
